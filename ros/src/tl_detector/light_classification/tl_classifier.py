f29fec0552e8fe2d7a3efaf40faa7cdfb3827b70
Ilya Stepanov <dev@ilyastepanov.com>
2018-08-27 00:54:09 -0700

    Traffic light classifier.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,44 @@
+import os
+
 from styx_msgs.msg import TrafficLight
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import tensorflow.contrib.slim.nets
+
+
+CHECKPOINT = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'vgg_16/model',
+)
+
+VGG_MEAN = [123.68, 116.78, 103.94]
+
+CLASSES = [TrafficLight.GREEN, TrafficLight.RED, TrafficLight.YELLOW]
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.input_image = tf.placeholder(tf.float32, shape=(600, 800, 3))
+
+            image = tf.image.resize_image_with_crop_or_pad(self.input_image, 224, 224)
+            image = image - tf.reshape(tf.constant(VGG_MEAN), [1, 1, 3])
+
+            images = tf.expand_dims(image, axis=0)
+
+            vgg = tf.contrib.slim.nets.vgg
+            with slim.arg_scope(vgg.vgg_arg_scope()):
+                logits, _ = vgg.vgg_16(images, num_classes=3, is_training=False)
+
+            saver = tf.train.Saver()
+
+            self.prediction = tf.nn.softmax(logits)
+
+        self.session = tf.Session(graph=self.graph)
+
+        saver.restore(self.session, CHECKPOINT)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +50,8 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+        result = self.session.run(self.prediction, feed_dict={self.input_image: image}).tolist()[0]
+        class_index = result.index(max(result))
+
+        return CLASSES[class_index]
