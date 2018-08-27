f241fe45f54766ffd2c955c1ab9e9cbb76d01662
Ilya Stepanov <dev@ilyastepanov.com>
2018-08-26 21:18:40 -0700

    Manually cherry-pick changes from https://github.com/istepanov/CarND-Capstone/pull/3 made by chaitanyar56 <chaitanyar5atp@gmail.com>.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,28 +10,24 @@ from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
-import math
-import uuid
-import numpy as np
+
 from scipy.spatial import KDTree
-import rospkg
-import os
+
 
 STATE_COUNT_THRESHOLD = 3
-LIGHT_DISTANCE_THRESHOLD = 50
-
 
 class TLDetector(object):
     def __init__(self):
-        rospy.init_node('tl_detector', log_level=rospy.DEBUG)
+        rospy.init_node('tl_detector')
 
         self.pose = None
-        self.previous_pose = None
         self.waypoints = None
-        self.waypoints_2d = None
-        self.waypoint_tree = None
         self.camera_image = None
         self.lights = []
+
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -63,13 +59,12 @@ class TLDetector(object):
         rospy.spin()
 
     def pose_cb(self, msg):
-        self.previous_pose = self.pose
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
         if not self.waypoints_2d:
-            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoints_2d = [[waypoint.pose.pose.position.x,waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
@@ -78,10 +73,8 @@ class TLDetector(object):
     def image_cb(self, msg):
         """Identifies red lights in the incoming camera image and publishes the index
             of the waypoint closest to the red light's stop line to /traffic_waypoint
-
         Args:
             msg (Image): image from car-mounted camera
-
         """
         self.has_image = True
         self.camera_image = msg
@@ -110,76 +103,62 @@ class TLDetector(object):
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
             pose (Pose): position to match a waypoint to
-
         Returns:
             int: index of the closest waypoint in self.waypoints
-
         """
-
-        return self.waypoint_tree.query([x, y], 1)[1]
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
-
         Args:
             light (TrafficLight): light to classify
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        #if(not self.has_image):
+        #    self.prev_light_loc = None
+        #    return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        #return self.light_classifier.get_classification(cv_image)
+        return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
             location and color
-
         Returns:
             int: index of waypoint closes to the upcoming stop line for a traffic light (-1 if none exists)
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        light_distance = float('inf')
-        stop_line_idx = -1
         closest_light = None
+        line_wp_idx = None
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if not None in (self.pose, self.previous_pose):
-            for idx, light in enumerate(self.lights):
-                stop_line_position = stop_line_positions[idx]
+        if(self.pose):
+            car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
-                diff_x = self.pose.pose.position.x - stop_line_position[0]
-                diff_y = self.pose.pose.position.y - stop_line_position[1]
-                distance = math.sqrt(diff_x * diff_x + diff_y * diff_y)
-
-                previous_car_vect = np.array([self.previous_pose.pose.position.x, self.previous_pose.pose.position.y])
-                car_vect = np.array([self.pose.pose.position.x, self.pose.pose.position.y])
-                light_vect = np.array([stop_line_position[0], stop_line_position[1]])
-
-                val = np.dot(light_vect - car_vect, car_vect - previous_car_vect)
-                if val > 0 and distance < light_distance:
-                    light_distance = distance
+            #TODO find the closest visible traffic light (if one exists)
+            diff = len(self.waypoints.waypoints)
+            for i, light in enumerate(self.lights):
+                #Get the stop line waypoint index
+                line = stop_line_positions[i]
+                temp_wp_idx = self.get_closest_waypoint(line[0],line[1])
+                # Find closest stop line waypoint index
+                d = temp_wp_idx - car_wp_idx
+                if d>= 0 and d< diff:
+                    diff = d
                     closest_light = light
-                    stop_line_idx = idx
+                    line_wp_idx = temp_wp_idx
 
+            if closest_light:
+                state = self.get_light_state(closest_light)
+                return line_wp_idx, state
 
-        if closest_light:
-            if light_distance < LIGHT_DISTANCE_THRESHOLD:
-                rospy.logdebug("next %s light @ %s m distance", str(closest_light.state), light_distance)
-                light_state = self.get_light_state(closest_light)
-                stop_line_position = stop_line_positions[stop_line_idx]
-                closest_light_wp_idx = self.get_closest_waypoint(stop_line_position[0], stop_line_position[1])
-                return closest_light_wp_idx, light_state
-
-        return -1, TrafficLight.UNKNOWN
+            return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
     try:
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -3,32 +3,28 @@ from lowpass import LowPassFilter
 from yaw_controller import YawController
 import rospy
 
+
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+
 class Controller(object):
-    def __init__(
-        self,
-        vehicle_mass,
-        fuel_capacity,
-        brake_deadband,
-        decel_limit,
-        accel_limit,
-        wheel_radius,
-        wheel_base,
-        steer_ratio,
-        max_lat_accel,
-        max_steer_angle):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius,
+        wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
 
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
-        self.throttle_controller = PID(0.3, #kp
-        0.1, #ki
-        0, #kd
-        0, #minimum throttle
-        0.2) #maximum throttle
 
-        self.vel_lpf = LowPassFilter(0.5, #tau
-        0.02) #ts
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0.#0. #Minimum throttle value
+        mx = 0.2#0.2 #Maximum throttle value
+
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
+        ts = 0.02 # Sampling time
+        self.vel_lpf = LowPassFilter(tau, ts)
 
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
@@ -39,13 +35,23 @@ class Controller(object):
 
         self.last_time = rospy.get_time()
 
+
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
 
         if not dbw_enabled:
             self.throttle_controller.reset()
-            return 0.0, 0.0, 0.0
+            return 0.,0.,0.
 
         current_vel = self.vel_lpf.filt(current_vel)
+
+        #rospy.loginfo("Angular vel: {}".format(angular_vel))
+        #rospy.logwarn("Target vel: {0}".format(linear_vel))
+        #rospy.logwarn("Target Ang vel: {0}\n".format(angular_vel))
+        #rospy.logwarn("Current vel: {0}".format(current_vel))
+        #rospy.logwarn("Filtered vel: {0}".format(self.vel_lpf.get()))
+
+
+
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
         vel_error = linear_vel - current_vel
@@ -60,13 +66,13 @@ class Controller(object):
 
         if linear_vel == 0 and current_vel < 0.1:
             throttle = 0
-            brake = 400 # should be 700 for Carla
-        elif throttle < 0.1 and vel_error < 0:
+            brake = 400 #N*m - to hold the car in place if we are stopped at a traffic light - Acceleration - 1m/s^2
+
+        elif  throttle < 0.1 and vel_error < 0: #vel_error < 0: #throttle < .1 and
             throttle = 0
             decel = max(vel_error, self.decel_limit)
+            #decel = max(throttle, self.decel_limit)
+            #throttle = 0
             brake = abs(decel) * self.vehicle_mass * self.wheel_radius
 
         return throttle, brake, steering
-
-
-
